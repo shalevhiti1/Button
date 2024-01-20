991923a5bee4f97c10d1f009e1c1c0a47c99f5de
shalevhiti1 <tayger110807@gmail.com>
2024-01-20 21:14:31 +0200

    button

--- app/src/main/java/com/example/button/MainActivity.java
+++ app/src/main/java/com/example/button/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView=(TextView) findViewById(R.id.textView);
+    }
+    public void updateText(View v){
+        textView.setText("Button clicked!");
     }
 }
